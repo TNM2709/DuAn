807e0fb97e006f6aa45ce318c2f153d0271d4580
unknown <adsf>
2024-11-16 15:04:28 +0700

    Add

--- saleapp/app/dao.py
+++ saleapp/app/dao.py
@@ -1,11 +1,13 @@
-from app.models import Category, Product
+from wheel.cli.pack import pack
 
+from app.models import Category, Product
+from app import app
 
 def load_categories():
     return Category.query.order_by("id").all()
 
 
-def load_products(cate_id=None, kw=None):
+def load_products(cate_id=None, kw=None, page=1):
     query = Product.query
 
     if cate_id:
@@ -14,4 +16,11 @@ def load_products(cate_id=None, kw=None):
     if kw:
         query = query.filter(Product.name.contains(kw))
 
+    page_size = app.config['PAGE_SIZE']
+    start = (page - 1) * page_size
+    query = query.slice(start, start + page_size)
+
     return query.all()
+
+def count_product():
+    return Product.query.count()
--- saleapp/app/index.py
+++ saleapp/app/index.py
@@ -1,3 +1,5 @@
+import math
+
 from flask import render_template, request
 import dao
 from app import app
@@ -6,12 +8,13 @@ from app import app
 @app.route("/")
 def index():
     cates = dao.load_categories()
-
     cate_id = request.args.get('category_id')
     kw = request.args.get('kw')
-    prods = dao.load_products(cate_id=cate_id, kw=kw)
-
-    return render_template('index.html', categories=cates, products=prods)
+    page = request.args.get('page', 1)
+    prods = dao.load_products(cate_id=cate_id, kw=kw, page=int(page))
+    totals = dao.count_product()
+    page_size = app.config['PAGE_SIZE']
+    return render_template('index.html', categories=cates, products=prods, pages=math.ceil(totals/page_size))
 
 
 if __name__ == '__main__':
